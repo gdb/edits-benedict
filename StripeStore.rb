804af7e04bf875db2614c85cc2792a45fb03e344
Siddarth Chandrasekaran <chandrasekaran.siddarth@gmail.com>
2011-02-05 15:58:34 -0800

    StripeStore commented.

--- StripeStore.rb
+++ StripeStore.rb
@@ -3,10 +3,14 @@ require 'StripeEmail'
 
 class StripeStore
     @@log = Logger.new('stripe_email.log')
+
+    # Delete from pstore
     def delete(email)
+        raise "Invalid argument to initialize: StripeEmail expected" unless email.class.to_s == "StripeEmail"
         @stripe_store.delete(email.subject)
     end
-    
+   
+    # Insert into pstore
     def insert(email)
         raise "Invalid argument to initialize: StripeEmail expected" unless email.class.to_s == "StripeEmail"
         @@log.debug "Received an insert query: #{email.from} -> #{email.to} [#{email.subject}]"
@@ -19,11 +23,13 @@ class StripeStore
         @@log.debug('Initializing pstore.')
         @stripe_store = PStore.new("stripe.store")
     end
-    
+
+    # Get all emails currently stored
     def emails
         @stripe_store.transaction { return @stripe_store.roots }
     end
 
+    # Get a specific email based on subject
     def get_email(subject) 
         @stripe_store.transaction { return @stripe_store[subject] }
     end
